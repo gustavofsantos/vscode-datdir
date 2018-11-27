de4578c0772a106e084f2e5132ecb1457af4d7eb
Gustavo Fernandes dos Santos <gfdsantos@inf.ufpel.edu.br>
2018-11-27 13:42:06 -0200

    improvements

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 
 ---
 
+## How it works
+
+When you share a directory, all subdirectories and files are attached to Dat, a link is generated and you can use it to clone all files in another directory in the same computer or another computer. You can also access with BeakerBrowser.
+
+The shared link still valid while your editor are open.
+
+To clone a directory with dat, just paste the Dat link (dat://) or directly the hash.
+
 ## Theres two commands
 
 * Dat Share: Share the actual directory or whatever other directory that exists.
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,14 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 const Dat = require('dat-node');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "dat-dir" is now active!');
-
     const dat = new DatHelper();
-   
+
     let datShare = vscode.commands.registerCommand('extension.datShare', () => {
         const rootPath = vscode.workspace.rootPath || '';
-        
+
         vscode.window.showInputBox({
             value: rootPath,
             placeHolder: 'Path to the directory to share'
@@ -26,10 +18,10 @@ export function activate(context: vscode.ExtensionContext) {
             }
         });
     });
-    
+
     let datClone = vscode.commands.registerCommand('extension.datClone', () => {
         const rootPath = vscode.workspace.rootPath || '';
-        
+
         vscode.window.showInputBox()
         .then(key => {
             if (key) {
@@ -62,7 +54,7 @@ class DatHelper {
             if (!err) {
                 dat.importFiles();
                 dat.joinNetwork();
-                
+
                 vscode.window.showInformationMessage(`dat://${dat.key.toString('hex')}`);
                 vscode.window.createOutputChannel('DatDir').appendLine(`dat://${dat.key.toString('hex')}`);
             }
